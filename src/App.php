af0c885afe0ea71e6917a31a8c4b40c744c6b707
Danil <my@ro2005.ru>
2021-10-05 00:58:59 +0700

    add new routes

--- src/App.php
+++ src/App.php
@@ -1,6 +1,7 @@
 <?php
 namespace Imagenator\Main;
 use Imagenator\Main\Router;
+use Ramsey\Uuid\Uuid;
 
 class App
 {
@@ -8,14 +9,21 @@ class App
     {
         $rout = new Router();
 
-        $rout->addRoute('/', ['FirstController', 'form']);
-        $rout->addRoutePost('/', ['FirstController', 'post']);
+        $rout->addRoute('/', ['UploadController', 'showPage']);
+        $rout->addRoute('/upload', ['UploadController', 'uploadPage']);
+        $rout->addRoutePost('/upload', ['UploadController', 'upload']);
+        $rout->addRoutePost('/result', ['UploadController', 'result']);
+
+        $rout->addRoute('/form', ['FirstController', 'form']);
+        $rout->addRoutePost('/form', ['FirstController', 'post']);
         $rout->addRoute('/blablabla', ['SecondController', 'blablabla']);
         $rout->addRoute('/hellYeah', function ($response, $request){
             $method = $request->query->get('method', 'print');
             if ($method !== "print") {
-                return $response->redirect("https://google.com", 400);
+                return $response->redirect("https://google.com", 302);
             }
+            $uuid = Uuid::uuid4();
+            dump($uuid);
             dump($method);
             dump($response);
             dump($request);
