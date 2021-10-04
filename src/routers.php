26680304e1d138c04d53828f611b0cb78c413fb1
Danil <my@ro2005.ru>
2021-10-04 18:59:24 +0700

    add /hellYeah route, using function controller

--- src/routers.php
+++ src/routers.php
@@ -2,3 +2,6 @@
 
 $router->addRoute('/', ['IndexController', 'form']);
 $router->addRoutePost('/', ['IndexController', 'post']);
+$router->addRoute('/hellYeah', function ($req){
+    dump($req);
+});
