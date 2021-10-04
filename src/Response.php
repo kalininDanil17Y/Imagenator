089c4a24ea27383e0698aa547f43289254c89d9e
Danil <my@ro2005.ru>
2021-10-04 21:48:43 +0700

    Add Response.php

--- src/Response.php
+++ src/Response.php
@@ -14,22 +14,31 @@ class Response extends View
     public function view($templateName, $params = [])
     {
         $responseHTML = $this->printTemplate($templateName, $params);
+        $this->response['body'] = $responseHTML;
         return $this;
     }
 
     public function setHeader($headName, $headValue = '')
     {
+        header($headName . ": " . $headValue);
         $this->response['header'][$headName] = $headValue;
         return $this;
     }
 
     public function setStatus($par1 = 200, $par2 = null){
         if ($par2 == null) {
+            http_response_code($par1);
             $this->response['code'] = $par1; //Set code
         } else {
-            $this->response['response'] = $par1; //Set message
+            http_response_code($par2);
+            $this->response['body'] = $par1; //Set message
             $this->response['code'] = $par2; //Set code
         }
+        return $this;
+    }
+
+    public function setBody($text){
+        $this->response['body'] = $text;
         return $this;
     }
 
