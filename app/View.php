acebd94f71ead2ad3c9087d60a25fcb4e6926fad
Danil <my@ro2005.ru>
2021-10-06 23:13:05 +0700

    add _text() function and add _text() function from Twig

--- app/View.php
+++ app/View.php
@@ -3,6 +3,7 @@ namespace App\Imagenator;
 
 use Twig\Loader\FilesystemLoader;
 use \Twig\Environment;
+use Twig\TwigFunction;
 
 /**
  * Class View
@@ -16,12 +17,43 @@ class View
     private $twig;
 
     /**
+     * @param $string
+     * @return array|mixed
+     */
+    public static function _text($string){
+        $words = explode(".", $string);
+        $langFile = __DIR__ . "/../translations/" . $words[0] . ".php";
+        if (!file_exists($langFile)) {
+            return $string;
+        }
+        $langarr = include($langFile);
+        if (!is_array($langarr)) {
+            return $string;
+        }
+        unset($words[0]);
+        foreach ($words as $key) {
+            if (is_array($langarr) && array_key_exists($key, $langarr)) {
+                $langarr = $langarr[$key];
+            } else {
+                return $string;
+            }
+        }
+
+        return $langarr;
+    }
+
+    /**
      *
      */
     public function viewInit()
     {
         $loader = new FilesystemLoader(__DIR__ . '/../templates/');
         $this->twig = new Environment($loader);
+
+        $_text = new TwigFunction('_text', function ($string = "") {
+            return $this->_text($string);
+        });
+        $this->twig->addFunction($_text);
     }
 
     /**
