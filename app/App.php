75743da8037d749c46aae74b2433f16690b274ef
root <root@Danilo9.localdomain>
2021-10-09 02:25:45 +0700

    del Dotenv

--- app/App.php
+++ app/App.php
@@ -4,7 +4,6 @@ namespace App\Imagenator;
 use App\Imagenator\Router;
 use App\Imagenator\Database;
 use Ramsey\Uuid\Uuid;
-use Dotenv\Dotenv;
 
 /**
  * Class App
@@ -15,22 +14,29 @@ class App
 
     public function init()
     {
-        $dotenv = Dotenv::createImmutable(__DIR__ . "/../");
-        $dotenv->load();
+        /*
+         * Соеденияемся с Базой
+         */
+        $database = new Database();
+        $database->connect();
 
-        new Database();
-
+        /*
+         * Создаём роутер
+         */
         $router = new Router();
 
-        $router->addRoute('/', ['UploadController', 'showPage']);
-        $router->addRoute('/upload', ['UploadController', 'uploadPage']);
-        $router->addRoutePost('/upload', ['UploadController', 'upload']);
-        $router->addRoutePost('/result', ['UploadController', 'resultPage']);
+        /*
+         * Указываем пути роутера
+         */
+        $router->addRoute('GET', '/', ['UploadController', 'showPage']);
+        $router->addRoute('GET', '/upload', ['UploadController', 'uploadPage']);
+        $router->addRoute('POST', '/upload', ['UploadController', 'upload']);
+        $router->addRoute('POST', '/result', ['UploadController', 'resultPage']);
 
-        $router->addRoute('/form', ['FirstController', 'form']);
-        $router->addRoutePost('/form', ['FirstController', 'post']);
-        $router->addRoute('/blablabla', ['SecondController', 'blablabla']);
-        $router->addRoute('/hellYeah', function ($response, $request){
+        $router->addRoute('GET', '/form', ['FirstController', 'form']);
+        $router->addRoute('POST', '/form', ['FirstController', 'post']);
+        $router->addRoute('GET', '/blablabla', ['SecondController', 'blablabla']);
+        $router->addRoute('GET', '/hellYeah', function ($response, $request){
             $method = $request->query->get('method', 'print');
             if ($method !== "print") {
                 return $response->redirect("https://google.com", 302);
